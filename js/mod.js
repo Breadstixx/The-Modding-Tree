f60a366d5f92171b4ad19858e75604ea4119c82b
Breadstixx <87549345+Breadstixx@users.noreply.github.com>
2024-05-11 09:57:24 -0400

    TreeofAscendancy 0.0
    First "build"

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "Tree of Ascendancy",
+	id: "AscendancyTree",
+	author: "Breadstixx",
+	pointsName: "A-points",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
